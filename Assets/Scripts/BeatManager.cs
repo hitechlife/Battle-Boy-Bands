f4dfbb7bebfd03d7fcd9f89788a3dec532720975
Joey Perrino <epiccrashv3@gmail.com>
2020-09-20 20:07:14 -0500

    Added a basic scene for testing which creates a beat according to the BPM given
    - BPM can be increased or decreased

---
+++ Assets/Scripts/BeatManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatManager : MonoBehaviour
+{
+    // Serialized fields
+    [SerializeField]
+    private int beatsPerMinute;
+    [SerializeField]
+    private AudioSource beatSound;
+    [SerializeField]
+    private bool playSound = true;
+
+    // Private variables - beat timer
+    private float measureTimer;
+
+    // Constants
+    private float SECONDS_CONST = 60;
+    private int SUBDIVISION_CONST = 4;
+
+    // Singleton reference
+    public static BeatManager S;
+
+    private void Awake()
+    {
+        S = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (measureTimer <= 0)
+        {
+            if (playSound)
+            {
+                SoundManager.S.PlaySound(beatSound);
+            }
+            measureTimer = 1.0f;
+        }
+
+        measureTimer -= (beatsPerMinute / SECONDS_CONST) * Time.deltaTime;
+        print(measureTimer);
+    }
+
+    public void ModifySoundValue(int bpmModifier)
+    {
+        int newBPM = beatsPerMinute + bpmModifier;
+
+        if (newBPM < 0)
+        {
+            newBPM = 0;
+        }
+
+        beatsPerMinute = newBPM;
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // Serialized fields
+    [SerializeField]
+    private int bpmIncrement = 20;
+
+    // Singleton reference
+    public static SoundManager S;
+
+    private void Awake()
+    {
+        S = this;
+    }
+
+    public void PlaySound(AudioSource soundToPlay)
+    {
+        soundToPlay.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            BeatManager.S.ModifySoundValue(bpmIncrement);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            BeatManager.S.ModifySoundValue(-bpmIncrement);
+        }
+    }
+}
