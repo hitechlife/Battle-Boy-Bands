a9aef29fac9311e4e152011cde296505b2bf0657
hitechlife <kanjur@andrew.cmu.edu>
2020-11-04 18:15:06 -0600

    Working on integrating game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    struct enemy {
+        public string name;
+        public bool defeated;
+    }
+    public static GameManager instance = null;
+    private static List<Opponent> opponents;
+
+    void Awake()
+    {
+        //check if instance exists
+        if (instance == null)
+        {
+            instance = this;
+            opponents.Add(new Opponent("Wash Depp", false));
+            opponents.Add(new Opponent("Kendrick Amore", false));
+            opponents.Add(new Opponent("Lil\' Pay", false));
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        // battleScript = GetComponent<BattleManager>();
+        // beatScript = GetComponent<BeatManager>();
+    }
+
+    // void newBoss()
+    // {
+    //     BossNum++;
+    //     InitLevel();
+    // }
+
+    public void InitLevel(string newScene)
+    {
+        //set up scene and stuff
+        SceneManager.LoadScene(newScene);
+    }
+}
--- Assets/Scripts/Opponent.cs
+++ Assets/Scripts/Opponent.cs
@@ -8,4 +8,10 @@ public class Opponent : MonoBehaviour
     //TODO: change to enum?
     public int difficulty;
     //TODO: insults already used
+    public bool defeated;
+
+    public Opponent(string name, bool defeated) {
+        this.name = name;
+        this.defeated = defeated;
+    }
 }
