f38cb6680611fea495fb30daf5dd390e7e670900
hitechlife <kanjur@andrew.cmu.edu>
2020-12-03 14:49:53 -0600

    accounting for lack of intro/end panels

---
+++ Assets/Scripts/MainMenuCrowdAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuCrowdAnimation : MonoBehaviour
+{
+    Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        if (GameManager.instance?.bossesDefeated == 3) StartCoroutine(AnimateCrowdMenu());
+    }
+
+    IEnumerator AnimateCrowdMenu() {
+        // crowd.SetActive(true);
+        object[] loadedSprite = Resources.LoadAll("Crowd", typeof(Sprite));
+        // FMOD.Studio.PLAYBACK_STATE state = FMOD.Studio.PLAYBACK_STATE.PLAYING;
+        // float timer = 0;
+        float animateInterval = 0.07f;
+        while (true) { // hardcoded len of crowd noise
+            foreach (object obj in loadedSprite) {
+                Sprite s = (Sprite)obj;
+                image.sprite = s;
+                // timer += animateInterval;
+                yield return new WaitForSeconds(animateInterval);
+            }
+            // timer += Time.deltaTime;
+            yield return null;
+            // Sound.getPlaybackState(out state);
+        }
+        // crowd.SetActive(false);
+    }
+}
